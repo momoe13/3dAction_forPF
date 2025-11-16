a852b15ad0a7b43d92ff8903b99f8229e889300c
momoe13 <c3084@oic.jp>
2025-11-16 23:02:25 +0900

    PLを常に追いかけて、PLの周りを回転する形に変更

--- Assets/Script/Character/CameraRotat.cs
+++ Assets/Script/Character/CameraRotat.cs
@@ -5,19 +5,19 @@ using UnityEngine.InputSystem;
 /*
  * プレイヤーを中心にカメラを回転させる
  * 参考：https://qiita.com/maid_chan/items/2cf76e76288b3657c8e9
- * https://qiita.com/OKUDAYUKI08/items/1bd658d9eaf5c39cd99a
  * **/
 public class CameraRotat : MonoBehaviour
 {
     [SerializeField]
-    private GameObject playerObject;         //追尾 オブジェクト
-    [SerializeField]
-    private Vector2 rotationSpeed;           //回転速度
-    private Vector3 lastMousePosition;      //最後のマウス座標
-    private Vector3 lastTargetPosition;     //最後の追尾オブジェクトの座標
+    GameObject playerObject;         //追尾 オブジェクト
+    [SerializeField, Range(0, 1)]
+    //private Vector2 rotationSpeed;           //回転速度
+    float rotationSpeed=1.0f;
+    Vector3 lastMousePosition;      //最後のマウス座標
+    Vector3 lastTargetPosition;     //最後の追尾オブジェクトの座標
 
 
-    private float zoom;
+    float zoom;
 
     void Start()
     {
@@ -28,40 +28,32 @@ public class CameraRotat : MonoBehaviour
 
     void Update()
     {
-
+        Chase();
         Rotate();
         Zoom();
     }
 
-
-    void Rotate()
+    //PL追従
+    void Chase()
     {
-
         transform.position += playerObject.transform.position - lastTargetPosition;
         lastTargetPosition = playerObject.transform.position;
-        //マウスカーソルで左右視点移動
-        float mx = Input.GetAxis("Mouse X");//カーソルの横の移動量を取得
-        if (Mathf.Abs(mx) > 0.001f) // X方向に一定量移動していれば横回転
-        {
-            transform.RotateAround(transform.position, Vector3.up, mx); // 回転軸はplayerオブジェクトのワールド座標Y軸
+    }
 
-        }
+    //マウスの左右移動で回転
+    void Rotate()
+    {
+        var newAngle = Vector3.zero;
+        //マウスの移動量記録
+        Vector3 nowMouseValue = Input.mousePosition - lastMousePosition;
 
+        //回転量計算
+        newAngle.x = rotationSpeed * nowMouseValue.x;
 
-        //if (Input.GetMouseButton(1))
-        //{
+        //PLを中心に回転（中心,回転軸,回転角度）
+        transform.RotateAround(playerObject.transform.position, Vector3.up, newAngle.x);
+        lastMousePosition = Input.mousePosition;
 
-        //    Vector3 nowMouseValue = Input.mousePosition - lastMousePosition;
-
-        //    var newAngle = Vector3.zero;
-        //    newAngle.x = rotationSpeed.x * nowMouseValue.x;
-        //    newAngle.y = rotationSpeed.y * nowMouseValue.y;
-
-        //    transform.RotateAround(playerObject.transform.position, Vector3.up, newAngle.x);
-        //    transform.RotateAround(playerObject.transform.position, transform.right, -newAngle.y);
-        //}
-
-        //lastMousePosition = Input.mousePosition;
     }
 
 
