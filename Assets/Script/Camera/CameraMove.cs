8994f893a4f5758efd825a31c9886b06d960f149
momoe13 <c3084@oic.jp>
2026-05-07 00:48:10 +0900

    スプライン上で動くようになったのでプッシュ
    次：入力のZ軸をスプライン上の移動に変換する

--- Assets/Script/Camera/CameraMove.cs
+++ Assets/Script/Camera/CameraMove.cs
@@ -1,24 +1,49 @@
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Splines;
 
 public class CameraMove : MonoBehaviour
-{ 
-    // スプライン
-    [SerializeField] private SplineContainer _splineContainer;
+{
+    [SerializeField] CinemachineSplineDolly cinemachineSplineDolly;
+    float dollSpeed = 1;
 
-    // スプラインに沿って移動させる対象
-    [SerializeField] private Transform _followTarget;
 
-    // 補間の割合
-    [SerializeField, Range(0, 1)] private float _percentage;
+    Vector3 cameraSpeed;
 
-    private void Update()
+
+    //ロックオン機能
+    [SerializeField] bool isRockon = true;
+
+    void Update()
     {
-        // 念のためNullチェック
-        if (_splineContainer == null || _followTarget == null)
-            return;
+        if (cinemachineSplineDolly == null) return;
 
-        // 計算した位置（ワールド座標）をターゲットに代入
-        _followTarget.position = _splineContainer.EvaluatePosition(_percentage);
+        // スプラインの長さを取得
+        float splineLength = cinemachineSplineDolly.Spline.CalculateLength();
+
+        // 経路上の現在位置（0〜1）を時間経過で進める
+        float deltaDistance = dollSpeed * Time.deltaTime;
+        float deltaT = deltaDistance / splineLength;
+
+        cinemachineSplineDolly.CameraPosition += deltaT;
+
+
     }
+
+
+    public void OnCamera(InputAction.CallbackContext context)
+    {
+        cameraSpeed = new Vector3(context.ReadValue<Vector2>().x, 0f, context.ReadValue<Vector2>().y);
+
+    }
+
+    public void OnRockon(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRockon = !isRockon;
+        }
+    }
+
 }
